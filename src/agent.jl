b895e2dffbfd823d7bdc5928b46ed3a262910be4
soblin <hilo.soblin@gmail.com>
2021-03-03 23:25:02 +0900

    added ch07_mcl_global

--- src/agent.jl
+++ src/agent.jl
@@ -47,7 +47,8 @@ function decision(
     estimator = agent.estimator_
     motion_update(estimator, agent.prev_v_, agent.prev_ω_, agent.dt)
     agent.prev_v_, agent.prev_ω_ = agent.v_, agent.ω_
-    if typeof(agent.estimator_) == Mcl || typeof(agent.estimator_) == KdlMcl
+    if typeof(agent.estimator_) == Mcl || typeof(agent.estimator_) == KdlMcl ||
+        typeof(agent.estimator_) == GlobalMcl
         observation_update(agent.estimator_, observation, envmap; resample = resample)
     elseif typeof(agent.estimator_) == KalmanFilter
         observation_update(agent.estimator_, observation)
