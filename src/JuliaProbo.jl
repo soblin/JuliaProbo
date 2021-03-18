d90fc16641952dfdbd76bffd0f61eeb42745798d
soblin <hilo.soblin@gmail.com>
2021-03-17 22:43:29 -0700

    checked graphslam6

--- src/JuliaProbo.jl
+++ src/JuliaProbo.jl
@@ -19,7 +19,7 @@ export IdealRobot, RealRobot, state_transition
 export IdealCamera, RealCamera, PsiCamera, observations, observation_function
 export Landmark, EstimatedLandmark, Map, World, push!, getindex
 export Particle,
-    copy, Mcl, KalmanFilter, KldMcl, ResetMcl, AMcl, motion_update, matQ, matH, matM, matA
+    copy, Mcl, KalmanFilter, KldMcl, ResetMcl, AMcl, motion_update, matQ, matH, matM, matA, matF
 export MapParticle, FastSlam1, FastSlam2
 
 # `include` order does matter(needs to be topologically sorted based on the type definition dependency)
