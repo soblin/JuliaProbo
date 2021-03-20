c8c9a91620024a17f112feb019b7ccd4db8f23f2
soblin <hilo.soblin@gmail.com>
2021-03-20 19:19:35 +0900

    added resample flag for better coverage in ch07_test.jl

--- test/ch07_test.jl
+++ test/ch07_test.jl
@@ -20,7 +20,7 @@
         annota = "t = $(round(t, sigdigits=3))[s]"
         z = observations(robot.sensor_, robot.pose_; noise = true, bias = true)
         p = draw(world, annota)
-        v, ω = decision(circling_agent, z, envmap)
+        v, ω = decision(circling_agent, z, envmap; resample=true)
         state_transition(robot, v, ω, dt; move_noise = true, vel_bias_noise = true)
     end
     if GUI
@@ -206,7 +206,7 @@ end
         annota = "t = $(round(t, sigdigits=3))[s]"
         z = observations(robot.sensor_, robot.pose_; noise = true, bias = true)
         p = draw(world, annota)
-        v, ω = decision(circling_agent, z, envmap)
+        v, ω = decision(circling_agent, z, envmap; resample=true)
         state_transition(robot, v, ω, dt; move_noise = true, vel_bias_noise = true)
     end
     if GUI
