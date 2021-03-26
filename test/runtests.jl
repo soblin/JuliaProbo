57225a82c7a52dd24516b9cc7df594d45aa377b4
soblin <hilo.soblin@gmail.com>
2021-03-26 14:24:07 -0700

    removed comment from test/runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,11 +9,11 @@ GUI = false
 format("../src")
 format("../test")
 
-#=include("ch03_test.jl")
+include("ch03_test.jl")
 include("ch04_test.jl")
 include("ch05_test.jl")
 include("ch06_test.jl")
 include("ch07_test.jl")
 include("ch08_test.jl")
-include("ch09_test.jl")=#
+include("ch09_test.jl")
 include("ch10_test.jl")
