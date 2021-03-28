4dee5d82bce1a239f4cc2764215a52e57d8c8ba2
soblin <hilo.soblin@gmail.com>
2021-03-28 19:54:54 +0900

    removed JuliaFormatter in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,9 @@
 using JuliaProbo
 using Plots
 using Test
-using JuliaFormatter
 
 ENV["GKSwstype"] = "nul"
 GUI = false
-FORMATTER = true
-
-if FORMATTER
-    format("../src")
-    format_file("../test/ch03_test.jl")
-    format_file("../test/ch04_test.jl")
-    format_file("../test/ch05_test.jl")
-    format_file("../test/ch06_test.jl")
-    format_file("../test/ch07_test.jl")
-    format_file("../test/ch08_test.jl")
-    format_file("../test/ch09_test.jl")
-    # Somehow this file fails
-    # format_file("../test/ch10_test.jl")
-    format_file("../test/ch12_test.jl")
-end
 
 include("ch03_test.jl")
 include("ch04_test.jl")
